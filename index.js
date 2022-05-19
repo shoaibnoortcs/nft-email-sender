54bfb9732a387c677dfcdcbd0d6adbf64dd216df
Muhammad Shoaib Noor/IT/HO <shoaib.noor@tcs.com.pk>
2022-05-19 15:25:41 +0500

    cors added .

--- index.js
+++ index.js
@@ -5,9 +5,10 @@
     const bodyParser = require('body-parser');
     const compression = require('compression')
     const controller = require('./controller')
+    const cors = require('cors')
     require('dotenv').config()
 
-
+    app.use(cors())
     app.use(compression())
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
@@ -15,7 +16,8 @@
 
 
     const port = process.env.PORT
-    app.post("/email-send", controller.sendEmail)
+
+    app.post("/external/email-send", controller.sendEmail)
     app.listen(port, () => {
         console.log("Server has started!")
     });
